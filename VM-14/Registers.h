26a659ff0bb870e3f7cb28ef54adcb65b9285fc7
Roman <70532463+RCh-stack@users.noreply.github.com>
2022-08-20 20:05:16 +0500

    Update Registers.h

--- VM-14/Registers.h
+++ VM-14/Registers.h
@@ -19,8 +19,8 @@ public:
 
     // -- integer registers --
     class GRP
-	{
-		int32_t grp[16] { };
+    {
+	int32_t grp[16] { };
     public:
         void push_int(uint16_t reg_number, int32_t num) { grp[reg_number] = num; }
         void push_uint(uint16_t reg_number, uint32_t num) { grp[reg_number] = num; }
@@ -28,17 +28,17 @@ public:
         int32_t get_int(uint16_t reg_number) { return grp[reg_number]; }
         uint32_t get_uint(uint16_t reg_number) { return grp[reg_number]; }
 
-	} grp{ };
+    } grp{ };
 
 	// -- float registers --
-	class GRPF
-	{
-	    float grp[16] { };
+    class GRPF
+    {
+	float grp[16] { };
     public:
         void push_float(uint16_t reg_number, float num) { grp[reg_number] = num; }
         float get_float(uint16_t reg_number) { return grp[reg_number]; }
 
-	} grpf{ };
+    } grpf{ };
 };
 
 #pragma once
